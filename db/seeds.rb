54d5fe1c1d3d04f6f8fd67915df08afed610d11a
Shelby Switzer <shelby@voterheads.com>
2015-03-19 19:31:24 -0600

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,29 @@ subdomains = ["smelly", "twerky", "hurryupwithmydamn",
               "stale","crusty","buttery","sausage",
               "vegetariansausage","tofu","verynothealthy",
               "cold","warm","robot","connected","kanye",
-              "imastealyour","northwest", "kanye", "kimkardashian",
-              "craycray","illuminati"]
+              "imastealyour","northwest", "kimkardashian",
+              "craycray","illuminati", "fab", "naughty", "oreostuffed"]
 
-subdomains.each do |subdomain|
-  Subdomain.create name: subdomain
-end
+Subdomain.create name: "smelly", image_url: ""
+Subdomain.create name: "twerky", image_url: ""
+Subdomain.create name: "hurryupwithmydamn", image_url: ""
+Subdomain.create name: "stale", image_url: ""
+Subdomain.create name: "crusty", image_url: ""
+Subdomain.create name: "buttery", image_url: ""
+Subdomain.create name: "sausage", image_url: ""
+Subdomain.create name: "vegetariansausage", image_url: ""
+Subdomain.create name: "tofu", image_url: ""
+Subdomain.create name: "verynothealthy", image_url: ""
+Subdomain.create name: "cold", image_url: ""
+Subdomain.create name: "warm", image_url: ""
+Subdomain.create name: "robot", image_url: ""
+Subdomain.create name: "connected", image_url: ""
+Subdomain.create name: "kanye", image_url: ""
+Subdomain.create name: "imastealyour", image_url: ""
+Subdomain.create name: "northwest", image_url: ""
+Subdomain.create name: "kimkardashian", image_url: ""
+Subdomain.create name: "craycray", image_url: ""
+Subdomain.create name: "illuminati", image_url: ""
+Subdomain.create name: "fab", image_url: ""
+Subdomain.create name: "naughty", image_url: ""
+Subdomain.create name: "oreostuffed", image_url: ""
