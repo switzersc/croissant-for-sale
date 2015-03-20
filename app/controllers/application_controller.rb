c55ead619e06984c4b81c4c8aee3a3d8af760790
Shelby Switzer <shelby@voterheads.com>
2015-03-19 18:40:05 -0600

    add subdomain stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_subdomain
+    @current_subdomain = request.subdomains.last
+  end
+  helper_method :current_subdomain
 end
